304b735df942f223fd5b5e3d69af42afaf656e91
Djabir Idrissou <djabiridrissou@Djabirs-MacBook-Pro.local>
2025-07-09 10:29:36 +0000

    fix

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,14 +1,15 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
 
-const AdminLayout = ({ children }: { children: React.ReactNode }) => {
+const AdminLayout = () => {
     return (
         <div className="min-h-screen bg-gray-50 flex flex-col">
             <Navbar />
             <main className="flex-grow">
-                {children}
+                <Outlet />
             </main>
-            
+
         </div>
     );
 }
